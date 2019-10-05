1b8dc1081e3563658e5144493ca4c43002a47b60
Anthony <gamerseth@protonmail.com>
2019-10-05 00:19:27 -0400

    v 0.1

--- main.py
+++ main.py
@@ -1,30 +1,50 @@
 from PIL import Image
 from os import listdir
 import sys
+import os
 path = "github/textToSymbols/images/"
 imagelist = listdir(path)
 stone = {}
+pgNumber = 1
 symbolsUsed = []
+print("give text")
 text = str(input())
-
+print("give title:")
+title = str(input())
+os.mkdir(path+title)
 for image in imagelist:
     dicValues = image.split(".")
     stone[dicValues[0]] = image
 
 text = text.split(" ")
-for word in text:
-    symbolsUsed.append(Image.open(path +stone[word]))
 
-widths, heights = zip(*(i.size for i in symbolsUsed))
-
-total_width = sum(widths)
-max_height = max(heights)
+pageWidth = 450
+pageHeight = 650
+total_width = pageWidth
+max_height = pageHeight
 
 new_im = Image.new('RGB', (total_width, max_height))
 
 x_offset = 0
-for im in symbolsUsed:
-  new_im.paste(im, (x_offset,0))
-  x_offset += im.size[0]
+y_offset = 0
+new_im.paste(Image.open(path+"page.jpg"), (0,0))
+for word in text:
+    im = Image.open(path +stone[word])
 
-new_im.save(path + "file.jpg")
+    if (x_offset > pageWidth - im.size[0]):
+      y_offset += im.size[1]
+      x_offset = 0
+    new_im.paste(im, (x_offset,y_offset))
+    print(im.size)
+    x_offset += im.size[0]
+
+    if (y_offset > pageHeight):
+      new_im.save(path+title+"/" + "page" + str(pgNumber) + ".jpg")
+      pgNumber += 1
+      y_offset = 0
+      x_offset = 0
+      new_im = Image.new('RGB', (total_width, max_height))
+      new_im.paste(Image.open(path+"page.jpg"), (0,0))
+    im.close()
+
+new_im.save(path+title+"/" + "page" + str(pgNumber) + ".jpg")
